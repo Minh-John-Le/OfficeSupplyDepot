8e4ba703af789a6a02b41e62b787c738cd235d46
Minh John Le <99804254+Minh-John-Le@users.noreply.github.com>
2023-03-12 07:09:22 -0700

    Initial commit

---
+++ OfficeSupplyDepotServlet/src/main/java/Controller/SignUpServlet.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+
+@WebServlet("/SignUpServlet")
+public class SignUpServlet extends HttpServlet {
+
+
+}
