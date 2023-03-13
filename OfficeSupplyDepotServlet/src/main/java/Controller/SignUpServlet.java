9c2620f488928cf46e8bbf7c6d24f179a4fd7371
Minh John Le <99804254+Minh-John-Le@users.noreply.github.com>
2023-03-12 21:58:24 -0700

    complete hook up to front to back to database

--- OfficeSupplyDepotServlet/src/main/java/Controller/SignUpServlet.java
+++ OfficeSupplyDepotServlet/src/main/java/Controller/SignUpServlet.java
@@ -16,20 +16,13 @@ import Beans.Customer;
 import DAO.CustomerDAO;
 
 @WebServlet("/signup")
-public class SignUpServlet extends HttpServlet {
-    
-    private CustomerDAO customerDAO; // assuming this has been initialized elsewhere
-    
+public class SignUpServlet extends HttpServlet {    
     public void init() throws ServletException {
     	
-        
-        
+          
     }
     
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	// Get the database connection details from the properties
-        //String driver = props.getProperty("db.driver");
-        
     	ServletContext context = getServletContext();
         
         // Get the input stream for the properties file
@@ -44,29 +37,19 @@ public class SignUpServlet extends HttpServlet {
 			e1.printStackTrace();
 		}
     	String url = props.getProperty("db.url");
-        String user = props.getProperty("db.username");
-        String pass = props.getProperty("db.password");
-        try {
-			input.close();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+        String mySQLuser = props.getProperty("db.username");
+        String mySQLpassword = props.getProperty("db.password");
+        CustomerDAO customerDAO = new CustomerDAO(url,mySQLuser, mySQLpassword);
+    	
         
-        try {
-			customerDAO = new CustomerDAO(url, user, pass);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	
-    	
+    	//=============================================
+        // Front end input receive
     	String username = request.getParameter("username");
         String password = request.getParameter("password");
         String name = request.getParameter("name");
         String email = request.getParameter("email");
         String account_type = request.getParameter("account-type");
-        System.out.println("account type = " + account_type);
+
         
         Customer customer = new Customer();
         customer.setUsername(username);
@@ -74,23 +57,14 @@ public class SignUpServlet extends HttpServlet {
         customer.setCustomerName(name);
         customer.setEmail(email);
         
-        try {
-            customerDAO.addCustomer(customer);
-            //response.sendRedirect("login.html"); // redirect to login page
-        } catch (Exception e) {
-            // handle exception
-        } finally {
-            try {
-				customerDAO.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-        }
+        //===============================================
+      
+		customerDAO.addCustomer(customer);
+	
+
     }
     
     public void destroy() {
-        customerDAO = null;
     }
 
 }
