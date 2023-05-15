478e0dcc059be8d72430bd70741851d1d11eb791
David Warshawsky <54288490+davidwarshawskysjsu@users.noreply.github.com>
2023-05-15 11:11:53 -0700

    checks if an email is valid

---
+++ OfficeSupplyDepotServlet/src/main/java/Validation/EmailValidation.java
@@ -0,0 +1,41 @@
+package Validation;
+
+import java.util.ArrayList;
+
+public class EmailValidation {
+
+    public static boolean isValid(Object value) {
+        if (value == null || !(value instanceof String)) {
+            return false;
+        }
+
+        String email = (String) value;
+        // Perform basic email validation
+        if (!email.matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}")) {
+            return false;
+        }
+        
+        return true;
+    }
+
+    public static ArrayList<String> getIssues(Object value) {
+        ArrayList<String> issues = new ArrayList<String>();
+
+        if (!(value instanceof String) || value == null) {
+            issues.add("Email address cannot be null.");
+            return issues;
+        }
+
+        String email = (String) value;
+        if (email.isEmpty()) {
+            issues.add("Email address cannot be empty.");
+        }
+
+        if (!email.matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}")) {
+            issues.add("Invalid email address format.");
+        }
+
+        return issues;
+    }
+
+}
